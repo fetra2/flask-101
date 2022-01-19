752992e226ac15d3144ab82954a1a58631b390b6
fetra rasolonjatovo <rasolonjatovofetra@gmail.com>
2022-01-19 12:41:51 +0300

    C:/Program Files/Git/api/v1/products test fixed

--- wsgi.py
+++ wsgi.py
@@ -9,10 +9,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/api/v1/produits')
-def produits():
+@app.route('/api/v1/products')
+def products():
     PRODUCTS = {
         1: { 'id': 1, 'name': 'Skello' },
         2: { 'id': 2, 'name': 'Socialive.tv' },
+        3: { 'id': 3, 'name': 'Le Wagon'},
     }
+    
+    
     return jsonify(PRODUCTS)
